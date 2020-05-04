5346a1ff03e7a162227332f79977753d2810a35e
livekeen <kevin@livekeen.com>
2020-05-04 18:50:12 -0400

    Added hover behaviour to about

---
+++ app/assets/scripts/functions.js
@@ -0,0 +1,19 @@
+// bug fixing: .css({'background-color': 'blue', 'opacity': '.5'})
+
+$(document).ready(function()
+  {
+    $('.content .content__item:first-child .content__actions').addClass("isActive"); // Adds action bar to all first nodes
+
+    $('.content__item').hover(
+      function(){ 
+        // $('.content .content__item:first-child .content__actions').removeClass("isActive"); // Hide all original action bars
+        $(this).addClass("isActive"); // Add an active class to the hovered area
+        $(this).children(".content__actions").addClass("isActive"); // Add an active class to the action bar
+      },
+      function() {
+        $(this).removeClass("isActive"); // Remove an active class to the hovered area
+        $(this).children(".content__actions").removeClass("isActive"); // Remove an active class to the action bar
+        $('.content .content__item:first-child .content__actions').addClass("isActive"); // Quickfix to prevent all original action bars from hiding on hover
+      }
+   )
+ });
